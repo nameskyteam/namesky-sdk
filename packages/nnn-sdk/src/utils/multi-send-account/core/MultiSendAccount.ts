fb9a046897773edc8440136a4d6a60af7c3fb5c7
sakura <zjchen1234@foxmail.com>
2022-11-23 14:02:35 +0800

    refactor

--- packages/nnn-sdk/src/utils/multi-send-account/core/MultiSendAccount.ts
+++ packages/nnn-sdk/src/utils/multi-send-account/core/MultiSendAccount.ts
@@ -2,11 +2,17 @@ import {Account} from "near-api-js";
 import {MultiTransaction} from "../../multi-transaction/core/MultiTransaction";
 import {parseOutcomeValue} from "../../multi-transaction/utils/outcome";
 import {AccountEnhancement} from "../types/enhancement";
+import {SignAndSendTransactionOptions} from "near-api-js/lib/account";
+import {FinalExecutionOutcome} from "near-api-js/lib/providers";
 
 /**
  * Enhancement of `Account` based on `MultiTransaction`
  */
 export class MultiSendAccount extends Account implements AccountEnhancement {
+  async signAndSendTransaction(options: SignAndSendTransactionOptions): Promise<FinalExecutionOutcome> {
+    return super.signAndSendTransaction(options)
+  }
+
   async multiSend<Value>(transaction: MultiTransaction): Promise<Value> {
     let outcome = null
     for (const nearApiJsTransaction of transaction.parseNearApiJsTransactions()) {
--- packages/nnn-sdk/src/utils/multi-send-account/types/enhancement.ts
+++ packages/nnn-sdk/src/utils/multi-send-account/types/enhancement.ts
@@ -1,5 +1,8 @@
 import {MultiTransaction} from "../../multi-transaction/core/MultiTransaction";
+import {FinalExecutionOutcome} from "near-api-js/lib/providers";
+import {SignAndSendTransactionOptions} from "near-api-js/lib/account";
 
 export interface AccountEnhancement {
+    signAndSendTransaction(options: SignAndSendTransactionOptions): Promise<FinalExecutionOutcome>
     multiSend<Value>(transaction: MultiTransaction): Promise<Value>
 }
