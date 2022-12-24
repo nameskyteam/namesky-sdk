a91b0dbb83dc8b4b8a635c78ee7c1d973bfe9888
sakura <zjchen1234@foxmail.com>
2022-12-24 20:52:14 +0800

    fix: lint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,12 +1,10 @@
 export default {
   "packages/namesky-sdk/**/*.ts": [
-    () => "tsc --project tsconfig.json --alwaysStrict",
     "npm run build",
     "prettier --write",
     "eslint --fix"
   ],
   "packages/example/**/*.ts": [
-    () => "tsc --project tsconfig.json --alwaysStrict",
     "prettier --write",
     "eslint --fix"
   ],
