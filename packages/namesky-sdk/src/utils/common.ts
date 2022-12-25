91c0ef74488b97b617de2200c2153f8933be0beb
sakura <zjchen1234@foxmail.com>
2022-12-25 14:21:26 +0800

    fix: remove unused

--- packages/namesky-sdk/src/utils/common.ts
+++ packages/namesky-sdk/src/utils/common.ts
@@ -1,5 +1,4 @@
 import { Amount } from './multi-transaction';
-import Big from 'big.js';
 import { base58CryptoHash, sha256 } from './crypto';
 
 export const REQUEST_ACCESS_PENDING_KEY_PREFIX = 'request_access_pending_key:';
@@ -8,24 +7,6 @@ export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.02);
 export const DEFAULT_APPROVAL_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.005);
 export const DEFAULT_MINT_FEE = Amount.parseYoctoNear(0.1);
 export const NUM_BYTES_DATA_LEN = 4;
-
-export function bigMax(...values: Big[]): Big {
-  return values.sort((a, b) => {
-    if (a.gt(b)) {
-      return -1;
-    }
-    return 1;
-  })[0];
-}
-
-export function bigMin(...values: Big[]): Big {
-  return values.sort((a, b) => {
-    if (a.gte(b)) {
-      return 1;
-    }
-    return -1;
-  })[0];
-}
 
 export function getBase58CodeHash(code: Buffer): string {
   const hash = sha256(code);
