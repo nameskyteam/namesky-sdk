59863fc5039554c6416576d00f8af4cfcd147838
sakura <zjchen1234@foxmail.com>
2023-07-02 18:39:54 +0800

    refactor: buildContractStateKeysRaw

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,22 +1,19 @@
-import { Amount } from 'multi-transaction';
-import { BigSource } from 'big.js';
+import { Amount, BorshSchema, BorshTypes, stringifyBorsh } from 'multi-transaction';
 
 export const REQUEST_ACCESS_PENDING_KEY_PREFIX = 'request_access_pending_key:';
 export const REGISTRANT_KEYSTORE_PREFIX = 'registrant:keystore:';
 export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.0125);
 export const DEFAULT_APPROVAL_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.005);
-export const NUM_BYTES_DATA_LEN = 4;
 export const FEE_DIVISOR = 10000;
 export const ACTION_MAX_NUM = 100;
 
 export function buildContractStateKeysRaw(state: { key: Buffer; value: Buffer }[]): Buffer {
-  return state.reduce((pre, { key }) => {
-    // 4 bytes for key len
-    const keyLen = Buffer.alloc(NUM_BYTES_DATA_LEN);
-    keyLen.writeUint32LE(key.length);
-    // borsh like, key_len + key_data
-    return Buffer.concat([pre, keyLen, key]);
-  }, Buffer.alloc(0));
+  const keys = state.map(({ key }) => key);
+  return stringifyBorsh(
+    BorshSchema.new(),
+    keys,
+    BorshTypes.FixedArray(BorshTypes.DynamicArray(BorshTypes.u8()), keys.length)
+  );
 }
 
 export function moveRegistrantPublicKeyToEnd(registrantPublicKey: string, publicKeys: string[]): string[] {
