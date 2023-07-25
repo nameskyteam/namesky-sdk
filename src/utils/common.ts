66da3394f2e30d918579e1af3f5942be15144b59
sakura <zjchen1234@foxmail.com>
2023-07-25 22:07:49 +0800

    chore: bump

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,8 +2,8 @@ import { Amount } from 'multi-transaction';
 
 export const REQUEST_ACCESS_PENDING_KEY_PREFIX = 'request_access_pending_key:';
 export const REGISTRANT_KEYSTORE_PREFIX = 'registrant:keystore:';
-export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.0125);
-export const DEFAULT_APPROVAL_STORAGE_DEPOSIT = Amount.parseYoctoNear(0.005);
+export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parse(0.0125, 'near');
+export const DEFAULT_APPROVAL_STORAGE_DEPOSIT = Amount.parse(0.005, 'near');
 export const FEE_DIVISOR = 10000;
 export const ACTION_MAX_NUM = 100;
 
