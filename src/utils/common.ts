b478ed7d217e99c7af06bf9e0cec6e08a8f97962
sakura <zjchen1234@foxmail.com>
2024-03-31 19:19:34 +0800

    chore: bump

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,6 +4,7 @@ import sha256 from 'sha256';
 import base58 from 'bs58';
 import { DAY_MS, MAX_TIMEOUT } from './constants';
 import { NameSkySpaceshipError } from '../errors';
+import { Buffer } from 'buffer';
 
 export function base58CodeHash(code: Buffer): string {
   const hash = Buffer.from(sha256(code), 'hex');
