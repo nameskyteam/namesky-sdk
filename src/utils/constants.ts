0bdd0365cb8af9fbc5e3df51fc315a051f8422da
sakura <zjchen1234@foxmail.com>
2024-04-22 21:50:19 +0800

    fix: storage deposit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ import { Amount } from 'multi-transaction';
 
 export const PENDING_REGISTRANT_ID_PREFIX = 'pending-registrant-id:';
 export const REGISTRANT_KEYSTORE_PREFIX = 'registrant:keystore:';
-export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parse(0.015, 'NEAR');
+export const DEFAULT_MARKET_STORAGE_DEPOSIT = Amount.parse(0.02, 'NEAR');
 export const DEFAULT_SPACESHIP_STORAGE_DEPOSIT = Amount.parse(0.02, 'NEAR');
 export const FEE_DIVISOR = 10000;
 export const ACTION_MAX_NUM = 100;
